7fade8b7d011c085304909c1cd1d1f4f51916a26
Jostein Kringlen <jostein.kringlen@finn.no>
2022-01-28 16:14:49 +0100

    add initial tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,21 +1,92 @@
-import { createCookieSessionStorage } from "@remix-run/server-runtime";
-import { MyStrategy } from "../src";
+import { createCookieSessionStorage } from '@remix-run/server-runtime';
 
-describe(MyStrategy, () => {
-  let verify = jest.fn();
-  // You will probably need a sessionStorage to test the strategy.
-  let sessionStorage = createCookieSessionStorage({
-    cookie: { secrets: ["s3cr3t"] },
-  });
+import { SpotifyStrategy } from '../src';
 
-  beforeEach(() => {
-    jest.resetAllMocks();
-  });
+describe(SpotifyStrategy, () => {
+    let verify = jest.fn();
+    // You will probably need a sessionStorage to test the strategy.
+    let sessionStorage = createCookieSessionStorage({
+        cookie: { secrets: ['s3cr3t'] },
+    });
 
-  test("should have the name of the strategy", () => {
-    let strategy = new MyStrategy({ something: "You may need" }, verify);
-    expect(strategy.name).toBe("change-me");
-  });
+    const strategyConf = {
+        clientID: 'CLIENT_ID',
+        clientSecret: 'CLIENT_SECRET',
+        callbackURL: 'https://example.app/callback',
+        sessionStorage,
+    };
 
-  test.todo("Write more tests to check everything works as expected");
+    let request: Request;
+    beforeEach(() => {
+        jest.resetAllMocks();
+        // jestFetchMock.resetMocks();
+        request = new Request('https://example.app/auth/spotify');
+    });
+
+    it('should allow changing the scope', async () => {
+        let strategy = new SpotifyStrategy(
+            {
+                ...strategyConf,
+                scope: 'custom',
+            },
+            verify,
+        );
+
+        try {
+            await strategy.authenticate(request, sessionStorage, {
+                sessionKey: 'user',
+            });
+        } catch (error) {
+            if (!(error instanceof Response)) throw error;
+            let location = error.headers.get('Location');
+
+            if (!location) throw new Error('No redirect header');
+
+            let redirectUrl = new URL(location);
+
+            expect(redirectUrl.searchParams.get('scope')).toBe('custom');
+        }
+    });
+
+    it('should have the scope `user-read-email` as default', async () => {
+        let strategy = new SpotifyStrategy(strategyConf, verify);
+
+        try {
+            await strategy.authenticate(request, sessionStorage, {
+                sessionKey: 'user',
+            });
+        } catch (error) {
+            if (!(error instanceof Response)) throw error;
+            let location = error.headers.get('Location');
+
+            if (!location) throw new Error('No redirect header');
+
+            let redirectUrl = new URL(location);
+
+            expect(redirectUrl.searchParams.get('scope')).toBe(
+                'user-read-email',
+            );
+        }
+    });
+
+    it('should correctly format the authorization URL', async () => {
+        let strategy = new SpotifyStrategy(strategyConf, verify);
+
+        try {
+            await strategy.authenticate(request, sessionStorage, {
+                sessionKey: 'user',
+            });
+        } catch (error) {
+            if (!(error instanceof Response)) throw error;
+
+            let location = error.headers.get('Location');
+
+            if (!location) throw new Error('No redirect header');
+
+            let redirectUrl = new URL(location);
+
+            expect(redirectUrl.hostname).toBe('accounts.spotify.com');
+            expect(redirectUrl.pathname).toBe('/authorize');
+        }
+    });
 });
